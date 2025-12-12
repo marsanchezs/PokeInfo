06a32b0d76525bc4ddeadc5377235c69d2ef9d6b
Mario Sánchez S. <marsanchezs@gmail.com>
2025-12-12 16:18:32 -0300

    network module

---
+++ app/src/main/java/cl/mess/pokeinfo/common/network/NetworkModule.kt
@@ -0,0 +1,55 @@
+package cl.mess.pokeinfo.common.network
+
+import cl.mess.pokeinfo.detail.data.source.remote.DetailService
+import cl.mess.pokeinfo.home.data.source.remote.HomeService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val BASE_URL = "https://pokeapi.co/api/v2/"
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHomeService(retrofit: Retrofit): HomeService =
+        retrofit.create(HomeService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideDetailService(retrofit: Retrofit): DetailService =
+        retrofit.create(DetailService::class.java)
+}
