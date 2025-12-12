40ea16b5f6cf5824774da7617c5352ed94cbe0ca
Mario Sánchez S. <marsanchezs@gmail.com>
2025-12-12 16:40:53 -0300

    add dependencies and poke info app

--- app/src/main/java/cl/mess/pokeinfo/MainActivity.kt
+++ app/src/main/java/cl/mess/pokeinfo/MainActivity.kt
@@ -3,45 +3,15 @@ package cl.mess.pokeinfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import cl.mess.pokeinfo.ui.theme.PokeInfoTheme
+import cl.mess.pokeinfo.navigation.NavHost
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            PokeInfoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            NavHost()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokeInfoTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/cl/mess/pokeinfo/PokeInfoApp.kt
@@ -0,0 +1,7 @@
+package cl.mess.pokeinfo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokeInfoApp  : Application()
